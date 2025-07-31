5363b2db1715497a2169b42a350562abbce8560b
Md Wasim Ansari <69861765+wasimm267@users.noreply.github.com>
2025-08-01 00:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! wasi made a change'
+    return 'Hello, world! wasim made a change'
 
 if __name__ == '__main__':
     app.run()
