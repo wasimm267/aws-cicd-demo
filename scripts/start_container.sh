8fa601277ed70a4511fb749f4b296fe3c568d360
Md Wasim Ansari <69861765+wasimm267@users.noreply.github.com>
2025-07-31 18:59:27 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
 
-echo "Starting Docker container from Docker Hub..."
+echo "Pulling the latest Docker image..."
+docker pull wasimm267/simple-flask-app
 
-# Pull the latest image
-docker pull wasimm267/simple-python-flask-app:latest
+echo "Checking for existing container named 'flask1'..."
+if [ "$(docker ps -aq -f name=flask1)" ]; then
+    echo "Stopping and removing existing container..."
+    docker stop flask1 || true
+    docker rm flask1 || true
+fi
 
-# Run the container
-docker run -d --name flask-app -p 5000:5000 wasimm267/simple-python-flask-app:latest
+echo "Starting a new container named 'flask1'..."
+docker run -itd --name flask1 -p 5000:5000 wasimm267/simple-flask-app
